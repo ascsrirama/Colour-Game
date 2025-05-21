683a5ea0964a59ea2d8d078487e6cd88b51203fe
Rama Anumanchipalli <sanu530@aucklanduni.ac.nz>
2025-05-21 16:03:46 +1200

    Passed Task 1 test 7 and 10 (somehow) by validating user inputs

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -3,6 +3,7 @@ package nz.ac.auckland.se281.engine;
 import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
 import nz.ac.auckland.se281.cli.Utils;
+import nz.ac.auckland.se281.model.Colour;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
@@ -19,20 +20,42 @@ public class Game {
 
     this.currentRound = 1;
     this.numRounds = numRounds;
-    MessageCli.START_ROUND.printMessage(currentRound, numRounds);
   }
 
   public void play() {
-    MessageCli.ASK_HUMAN_INPUT.printMessage();
-    //String input = Utils.scanner.nextLine();
-    if(currentRound <= numRounds) { 
-      MessageCli.START_ROUND.printMessage(currentRound, numRounds);
-      currentRound++;
-    } else { 
+   
 
+    // Showing the rounds 
+    if(currentRound > numRounds) { 
+      return;
     }
+      MessageCli.START_ROUND.printMessage(currentRound, numRounds);
 
-  }
+      //Boolean validInput = false;
+        while(true) {  
+          MessageCli.ASK_HUMAN_INPUT.printMessage();
+          String input = Utils.scanner.nextLine();
+          String [] parts = input.split(" ");
+
+          if (parts.length != 2) { 
+            MessageCli.INVALID_HUMAN_INPUT.printMessage();
+          continue;
+          }
+
+          Colour picked = Colour.fromInput(parts[0]);
+          Colour guess = Colour.fromInput(parts[1]);
+          if(picked == null || guess == null) { 
+            MessageCli.INVALID_HUMAN_INPUT.printMessage();
+            continue;
+          }
+
+          // Valid input received, break out of the loop
+          break;
+        }
+        
+      currentRound++;
+    }
+  
 
   public void showStats() {}
 }
