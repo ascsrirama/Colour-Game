b74cbaf35e29185cd9a951db02ceb25e3aacfc73
Rama Anumanchipalli <sanu530@aucklanduni.ac.nz>
2025-05-22 19:01:43 +1200

    Created Factory Class to implement Factory OOP

---
+++ src/main/java/nz/ac/auckland/se281/engine/StrategyFactory.java
@@ -0,0 +1,54 @@
+package nz.ac.auckland.se281.engine;
+
+import nz.ac.auckland.se281.Main.Difficulty;
+
+public class StrategyFactory {
+
+  public static AiStrategy createStrategy(Difficulty difficulty) {
+    // choose strategy
+    switch (difficulty) {
+      case EASY:
+        RandomStrategy randomStrategy = new RandomStrategy();
+        return randomStrategy;
+      case MEDIUM:
+        AvoidLastStrategy avoidLastStrategy = new AvoidLastStrategy();
+        return avoidLastStrategy;
+      case HARD:
+        LeastUsedStrategy leastUsedStrategy = new LeastUsedStrategy();
+        return leastUsedStrategy;
+      default:
+        throw new IllegalArgumentException("Unknown difficulty: " + difficulty);
+    }
+  }
+
+  public static AiStrategy getHardStrategy(int round, int lastAiScore, String currentStrategy) {
+    if (round == 1 || round == 2) {
+      return new RandomStrategy();
+    } else if (round >= 4) {
+      if (lastAiScore == 0) {
+        if ("Least Used".equals(currentStrategy)) {
+          return new AvoidLastStrategy();
+        } else {
+          return new LeastUsedStrategy();
+        }
+      } else {
+        return new LeastUsedStrategy();
+      }
+    }
+    return new LeastUsedStrategy(); // Default for round 3
+  }
+
+  public static String getStrategyName(AiStrategy strategy) {
+    if (strategy instanceof RandomStrategy) {
+      return "Random";
+    } else if (strategy instanceof AvoidLastStrategy) {
+      return "Avoid Last";
+    } else if (strategy instanceof LeastUsedStrategy) {
+      return "Least Used";
+    }
+    return "Unknown";
+  }
+
+
+
+}
